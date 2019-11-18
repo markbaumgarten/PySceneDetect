9ff657f521ce4de0bda06ad9cd523c1cccdd2582
Mark Baumgarten <mark.baumgarten@gmail.com>
2019-11-18 07:37:00 +0100

    New video_manager...old state?

--- scenedetect/video_manager.py
+++ scenedetect/video_manager.py
@@ -2,16 +2,16 @@
 #
 #         PySceneDetect: Python-Based Video Scene Detector
 #   ---------------------------------------------------------------
-#     [  Site: http://www.bcastell.com/projects/pyscenedetect/   ]
+#     [  Site: http://www.bcastell.com/projects/PySceneDetect/   ]
 #     [  Github: https://github.com/Breakthrough/PySceneDetect/  ]
 #     [  Documentation: http://pyscenedetect.readthedocs.org/    ]
 #
-# Copyright (C) 2012-2019 Brandon Castellano <http://www.bcastell.com>.
+# Copyright (C) 2014-2019 Brandon Castellano <http://www.bcastell.com>.
 #
 # PySceneDetect is licensed under the BSD 3-Clause License; see the included
 # LICENSE file, or visit one of the following pages for details:
 #  - https://github.com/Breakthrough/PySceneDetect/
-#  - http://www.bcastell.com/projects/pyscenedetect/
+#  - http://www.bcastell.com/projects/PySceneDetect/
 #
 # This software uses Numpy, OpenCV, click, tqdm, simpletable, and pytest.
 # See the included LICENSE files or one of the above URLs for more information.
@@ -24,7 +24,7 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-""" PySceneDetect `scenedetect.video_manager` Module
+""" Module: ``scenedetect.video_manager``
 
 This module contains the :py:class:`VideoManager` class, which provides a consistent
 interface to reading videos, specific exceptions raised upon certain error
@@ -46,11 +46,12 @@ respect to a SceneManager object.
 # consider rewriting an asynchronous frame grabber in C++ and write a C-API to
 # interface with the Python ctypes module. - B.C.
 
-
 # Standard Library Imports
 from __future__ import print_function
 import os
 import math
+import subprocess
+import numpy as np
 
 # Third-Party Library Imports
 import cv2
@@ -178,6 +179,16 @@ def get_num_frames(cap_list):
     """
     return sum([math.trunc(cap.get(cv2.CAP_PROP_FRAME_COUNT)) for cap in cap_list])
 
+def get_rotation(file_path):
+    # type: (str) -> str
+    """ Get Rotation: Returns the number of degrees a video has been rotated.
+
+    Uses subprocess to call external program ffprobe on the video.
+    """
+    cmd = '''ffprobe -loglevel error -select_streams v:0 -show_entries stream_tags=rotate \
+             -of default=nw=1:nk=1 -i "%s"''' % file_path
+    rotation = subprocess.getoutput(cmd)
+    return rotation or None
 
 def open_captures(video_files, framerate=None, validate_parameters=True):
     # type: (Iterable[str], float, bool) -> Tuple[List[VideoCapture], float, Tuple[int, int]]
@@ -232,6 +243,7 @@ def open_captures(video_files, framerate=None, validate_parameters=True):
 
     try:
         cap_list = [cv2.VideoCapture(video_file) for video_file in video_files]
+        rotation_list = [get_rotation(video_file) for video_file in video_files]
         video_names = [get_video_name(video_file) for video_file in video_files]
         closed_caps = [video_names[i] for i, cap in
                        enumerate(cap_list) if not cap.isOpened()]
@@ -258,7 +270,7 @@ def open_captures(video_files, framerate=None, validate_parameters=True):
         release_captures(cap_list)
         raise
 
-    return (cap_list, cap_framerate, cap_frame_size)
+    return (cap_list, cap_framerate, cap_frame_size, rotation_list)
 
 
 def release_captures(cap_list):
@@ -367,8 +379,8 @@ class VideoManager(object):
         if not video_files:
             raise ValueError("At least one string/integer must be passed in the video_files list.")
         # These VideoCaptures are only open in this process.
-        self._cap_list, self._cap_framerate, self._cap_framesize = open_captures(
-            video_files=video_files, framerate=framerate)
+        self._cap_list, self._cap_framerate, self._cap_framesize, self._rotation_list = \
+            open_captures(video_files=video_files, framerate=framerate)
         self._end_of_video = False
         self._start_time = self.get_base_timecode()
         self._end_time = None
@@ -377,6 +389,7 @@ class VideoManager(object):
         self._curr_cap, self._curr_cap_idx = None, None
         self._video_file_paths = video_files
         self._logger = logger
+        self._rotates = {"270": 1, "180": 2, "90": 3}
         if self._logger is not None:
             self._logger.info(
                 'Loaded %d video%s, framerate: %.2f FPS, resolution: %d x %d',
@@ -642,8 +655,9 @@ class VideoManager(object):
         self._started = False
         self._end_of_video = False
         self._curr_time = self.get_base_timecode()
-        self._cap_list, self._cap_framerate, self._cap_framesize = open_captures(
-            video_files=self._video_file_paths, framerate=self._curr_time.get_framerate())
+        self._cap_list, self._cap_framerate, self._cap_framesize, self._rotation_list = \
+            open_captures(video_files=self._video_file_paths,
+                          framerate=self._curr_time.get_framerate())
         self._curr_cap, self._curr_cap_idx = None, None
 
 
@@ -724,6 +738,7 @@ class VideoManager(object):
         if self._curr_cap is not None and self._end_of_video != True:
             while not retrieved:
                 retrieved, self._last_frame = self._curr_cap.retrieve()
+                self._rotate()
                 if not retrieved and not self._get_next_cap():
                     break
                 if self._downscale_factor > 1:
@@ -734,6 +749,11 @@ class VideoManager(object):
             self._last_frame = None
         return (retrieved, self._last_frame)
 
+
+    def _rotate(self):
+        rotate_degrees = self._rotation_list[self._curr_cap_idx]
+        if rotate_degrees:
+            self._last_frame = np.rot90(self._last_frame, self._rotates[rotate_degrees])
 
     def read(self):
         # type: () -> Tuple[bool, Union[None, numpy.ndarray]]
@@ -750,6 +770,7 @@ class VideoManager(object):
         if not self._started:
             raise VideoDecoderNotStarted()
 
+        read_frame = False
         if self._curr_cap is not None and self._end_of_video != True:
             read_frame, self._last_frame = self._curr_cap.read()
 
@@ -784,4 +805,3 @@ class VideoManager(object):
             self._curr_cap_idx += 1
             self._curr_cap = self._cap_list[self._curr_cap_idx]
             return True
-
